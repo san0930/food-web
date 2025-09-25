d4d403089b7fbdff5cd8c8abde231a8c58ac436b
san0930 <sanjaysrinivasan788206@gmail.com>
2025-09-25 07:47:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Simple scroll navigation
+const links = document.querySelectorAll('nav a');
+links.forEach(link => {
+  link.addEventListener('click', function(e) {
+    const href = link.getAttribute('href');
+    if (href.startsWith('#')) {
+      e.preventDefault();
+      document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
